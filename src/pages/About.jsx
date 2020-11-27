c493ef4e0b2024bc970732c112c926239a7398c8
AyoOluwa-Israel <izzyfresher@gmail.com>
2020-11-27 11:28:33 +0100

    initail commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section" >
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut, autem! Veniam incidunt deleniti nesciunt eum sunt. Ipsum accusamus reprehenderit neque esse! Sed nam aspernatur quaerat officia amet ipsa corporis enim quam repellat minima nobis dolorem deserunt illo, adipisci recusandae aliquam?
+      </p>
+    </section>
+  )
+}
+
+export default About
